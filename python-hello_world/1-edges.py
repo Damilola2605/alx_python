38147e623d2885a005cc572f115a64ca952a238b
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-20 20:50:53 +0100

    Making changes and i hope its correct

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -5,9 +5,4 @@ slice = word[7:]
 print("Last 2 letters: on")
 slice = word[1:8]
 print("Middle word: olberto")
-'''slice = word[:3]
-print(slice)
-slice = word[7:]
-print(slice)
-slice = word[1:8]
-print(slice)'''
+
