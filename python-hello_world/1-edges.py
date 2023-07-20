50f0383aee8674d257d17a97578c0a26c96627e2
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-20 20:36:49 +0100

    using slice and print

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,7 @@
 word = "Holberton"
-print("First 3 letters: {}".format(word_first_3))   slice = word[:3]
+slice = word[:3]
 print(slice)
-print("Last 2 letters: {}".format(word_last_2))     slice = word[7:]
+slice = word[7:]
 print(slice)
-print("Middle word: {}".format(middle_word))  slice = word[1:8]
+slice = word[1:8]
 print(slice)
