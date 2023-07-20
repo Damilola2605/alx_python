417ec56b148d74ab69884db9c71b105e879e5be0
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-20 20:27:01 +0100

    Making changes

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,12 +1,7 @@
 word = "Holberton"
-slice = word[:3]
+print("First 3 letters: {}".format(word_first_3))   slice = word[:3]
 print(slice)
-slice = word[7:]
+print("Last 2 letters: {}".format(word_last_2))     slice = word[7:]
 print(slice)
-slice = word[1:8]
+print("Middle word: {}".format(middle_word))  slice = word[1:8]
 print(slice)
-
-'''word = "Holberton"
-print("First 3 letters: {}".format(word_first_3))
-print("Last 2 letters: {}".format(word_last_2))
-print("Middle word: {}".format(middle_word))'''
