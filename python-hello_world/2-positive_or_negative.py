36efd2d98580422e70dc0022227b1e085598c1d0
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-21 08:18:54 +0100

    Hexadecimal printing

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,9 +1,9 @@
+#!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-
-#check if the number is positive, negative, or zero
-#if number > 0:
-isTrue = True
-print("0 > -10")
-#if number  < 0:
-   # print("The number is negative")
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
