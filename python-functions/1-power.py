064725c1106911197fa832fe7107c69958024f5d
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-21 19:54:12 +0100

    Print combo

---
+++ python-functions/1-power.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''def pow(a, b):
+    result = 1
+    for x in range(b):
+        result *= a
+print= pow(2, 3)'''
+#!/usr/bin/python3
+pow = __import__('1-power')
+def pow(a, b):
+    
+    print(pow(2, 2))
+print((98, 2))
+print((98, 0))
+print((100, -2))
+print((-4, 5))
--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-for num1 in range(0, 10):
+'''for num1 in range(0, 10):
     for num2 in range(num1 + 1, 10):
        # print("{:02d}, ".format(num1 * 10 + num2),end="")
-       print("{}{}".format(num1, + num2), end=", ")
+       print("{}{}".format(num1, + num2), end=", ")'''
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
