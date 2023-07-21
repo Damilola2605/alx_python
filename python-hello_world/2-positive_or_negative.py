7027e4eb8a81a10aded0ea8daf55b307bdf7f42d
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-21 07:50:37 +0100

    Using print

---
+++ python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,9 @@
+import random
+number = random.randint(-10, 10)
+
+#check if the number is positive, negative, or zero
+#if number > 0:
+isTrue = True
+print("0 > -10")
+#if number  < 0:
+   # print("The number is negative")
