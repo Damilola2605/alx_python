1abd40c52dec886133e88072eeb95db924cf7fe5
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-21 10:50:51 +0100

    Making correction

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for num1 in range(8):
-    for num2 in range(num1 + 1,9):
-        print("{:02d}, ".format(num1 * 10 + num2),end=" ")
+    for num2 in range(num1 + 1, 9):
+        print("{:02d}, ".format(num1 * 10 + num2),end="")
