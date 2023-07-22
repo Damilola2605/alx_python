80a93ad8262f1ac21e0e3d3409f7096b75f0df3d
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-23 00:44:30 +0100

    Prime number

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -3,3 +3,10 @@ def is_prime(number):
         return False
     elif number == 2:
         return True
+    
+    # Check divisibility from 2 to the square root of the number
+    for divisor in range(2, int(number**0.5) + 1):
+        if number % divisor == 0:
+            return False
+
+    return True
