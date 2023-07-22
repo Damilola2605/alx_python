6a9573f7c987f2a410c26dce3355f9d114e169cc
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-22 09:48:57 +0100

    commit and push

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-'''def pow(a, b):
+def pow(a, b):
     result = 1
     for x in range(b):
         result *= a
-print= pow(2, 3)'''
-#!/usr/bin/python3
-pow = __import__('1-power')
-def pow(a, b):
-    
-    print(pow(2, 2))
-print((98, 2))
-print((98, 0))
-print((100, -2))
-print((-4, 5))
+    return result
+result = pow(2, 3)
+print(result)
