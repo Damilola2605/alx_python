66b08c0939e802e5fe1ff0a9aa9b942124b90efd
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-23 00:29:58 +0100

    fibonacci

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,15 +1,13 @@
-def fibonacci_sequence(num):
-    num1 = 0
-    num2 = 1
-    next_number = 0
-    count = 1
-  
-    while(count <= num):
-        print(next_number, end=" ")
-        count += 1
-        num1 = num2
-        num2 = next_number
-        next_number = num1 + num2
-        t_number = num1 + num2
-
-print(fibonacci_sequence(10))
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        sequence = [0, 1]
+        while len(sequence) < n:
+            next_num = sequence[-1] + sequence[-2]
+            sequence.append(next_num)
+        return sequence
