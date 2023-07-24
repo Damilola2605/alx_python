9eaea1d31152e0259868a1f096d66633ceabe5ac
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-24 22:37:07 +0100

    Making changes

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    # Importing the variable 'a' from variable_load_2.py
-    from variable_load_2 import a
+ # Importing the variable 'a' from variable_load_2.py
+from variable_load_2 import a
 
-    # Printing the value of the variable 'a'
-    print(a)
+ # Printing the value of the variable 'a'
+print(a)
