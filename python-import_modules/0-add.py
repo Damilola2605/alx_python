584c0ddfd2b6ba50d15e40ff7de1d89e886dda5b
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-24 13:31:12 +0100

    Making changes

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+from add_0 import add
 
 a = 1
 b = 2
-result = __import__("add_0").add(a, b)
-print(f"{a} + {b} = {result}")
+
+result = add(a, b)
+
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, result))
