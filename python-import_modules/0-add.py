1615b4065000aa56a3c20bba8abd4317f546ad1f
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-24 12:45:12 +0100

    Making correction

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,6 +4,16 @@ from add_0 import add
 a = 1
 b = 2
 
+"""My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+
 result = add(a, b)
 
 if __name__ == "__main__":
