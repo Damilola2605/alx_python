a8f593c44a59b5db81b4e649a11ff450aae73513
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-25 10:10:17 +0100

    Making changes

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+# Import the add function from add_0.py
+from add_0 import add
 
 # Define a and b on separate lines
 a = 1
 b = 2
 
-# Import the add function from add_0.py
-from add_0 import add
-
-# Perform the addition operation using the add function and print the result
+result = add(a, b)
 print("{} + {} = {}".format(a, b, add(a, b)))
