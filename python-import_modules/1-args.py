983e6d8eabed450475c7f47c63f43a1ddc2c94b2
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-25 15:04:25 +0100

    Load variable

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,19 +2,9 @@
 import sys
 
 
-def print_arguments():
-    num_arguments = len(sys.argv) - 1
+def arguments():
+    num_arguments = len(sys.argv)
 
-    # Print the number of arguments and the appropriate suffix
-    if num_arguments == 1:
-        print("Number of argument: {}.".format(num_arguments))
-    else:
-        print("Number of arguments: {}.".format(num_arguments))
-
-    # Print the list of arguments and their positions
-    if num_arguments > 0:
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print("{}: {}".format(i, arg))
-
-if __name__ == "__main__":
-    print_arguments()
+    # Print the number of arguments
+    if num_arguments == 1: 
+        print ("Number of arguments:{}." format(num_arguments))
--- python-import_modules/variable_load_2.py
+++ python-import_modules/variable_load_2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+a = 98
 
-def variable(a):
-    return a
+"""Simple variable
+"""
