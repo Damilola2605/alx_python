b03d24fa36bdfc7d210c59b32f796917fdb6e6ce
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-25 15:36:07 +0100

    Load simple variable

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
-import importlib.util
-
-def main():
- module_name = "variable_load_2"
-
- # Load the module using importlib
- spec = importlib.util.spec_from_file_location(module_name, f"{module_name}.py")
- module = importlib.util.module_from_spec(spec)
- spec.loader.exec_module(module)
-
- # Access the variable 'a' and print its value
- print("The value of 'a' is:", module.a)
 
 if __name__ == "__main__":
- main()
+    from variable_load_2 import a
+    print(a)
