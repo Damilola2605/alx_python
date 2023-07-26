e3186676afec5e5ea338dd472d4abc91565d8d6f
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-26 14:06:45 +0100

    Print division

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_division(a ,b):
-    a = 12
-    b = 2
+
     try:
         result = a / b
-
     except ZeroDivisionError:
         result = None
-        
     finally:
         print("Inside result: {}".format(result))
         return result
