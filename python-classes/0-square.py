82710aeeb5cdddf67f082686af945662bbaad10f
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-07-31 21:19:17 +0100

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -30,9 +30,9 @@ class Square:
     Attributes:
         __size (int): The size of the square.
     """
-    
-def __init__(self, size):
-        #  Initializes a Square instance.
-        #  Args:
-        #    size (int): The size of the square.
+     def __init__(self, size):
+        ''' Initializes a Square instance.
+             Args:
+        size (int): The size of the square.
+        '''
         self.__size = size
