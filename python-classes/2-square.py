d1413ad83a2dd58165ceb21e191e1bd95eb103d1
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-01 13:34:11 +0100

    Access and update private attribute

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -24,31 +24,32 @@ Todo:
 
 
 class Square:
-     """
-     Represents a square.
-     Attributes:
-     
-     __size (int): The size of the square.
-     """
-     
-     def __init__(self, size=0):
-        ''' Initializes a Square instance.
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance.
+
         Args:
-        size (int, optional): The size of the square. Set to 0
-        '''
+            size (int, optional): The size of the square. Defaults to 0.
+        """
         self.__size = size
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
-        
-        def area(self):
-            
-            '''Represents the value of area
 
-            Returns:
-            int: the calculated square of the area
-            '''
-            return self.__size ** 2
-        
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
