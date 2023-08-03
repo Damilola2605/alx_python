3b8f777ff844968e90da45983e45d4fd11bd10e6
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-03 22:55:55 +0100

    Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -3,9 +3,20 @@
 Module on the BaseGeometry class.
 """
 
-
-class BaseGeometry:
+class TheMetaclass(type):
+    """
+    This class contain adefault parent obj.
+    """
+    def __dir__(subclass):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
+class BaseGeometry(metaclass=TheMetaclass):
     """
     An empty class file defining the base geometry.
+    """
+    def __dir__(subclass):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    """
+    Method to remove the initsubclass
     """
     pass
