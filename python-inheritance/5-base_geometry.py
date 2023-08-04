baa45b3ce75033629e7ce8ceb6972c643d053626
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-04 15:46:16 +0100

    Integer validator

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Module on the BaseGeometry class.
+"""
+
+class TheMetaclass(type):
+    """
+    This class contain adefault parent obj.
+    """
+    def __dir__(subclass):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
+class BaseGeometry(metaclass=TheMetaclass):
+    """
+    An empty class file defining the base geometry.
+    """
+    def __dir__(subclass):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    """
+    Method to remove the initsubclass
+    """
+    def area(self):
+
+        """
+        Calculate the area of the geometry.
+
+        Raises:
+            Exception: This method is not implemented.
+        """
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """
+        Validating integer value
+
+        Args:
+            name (str): The name of the value being validated.
+            value: The value to be validated.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
