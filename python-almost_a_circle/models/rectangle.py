1a097ae62e90bac88a8cedc32db9d379fcb3b46b
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-08 15:26:57 +0100

    First Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from models.base import Base
+"""
+Module on a Rectangle class that inherits from Base
+"""
+class Rectangle (Base):
+    """
+    This is the calss defining the Rectangle
+
+    Attribute include:
+    __width -> width
+    __height -> height
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        This function intializes a Instance of the Rectangle
+
+        Args:
+         width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+        x (int): The x-coordinate of the rectangle.
+        y (int): The y-coordinate of the rectangle.
+        id (int): The value for the id attribute.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
