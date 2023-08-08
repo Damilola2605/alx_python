80b7f8c80bbfa5325c7ccaaa16caefd1cfae4d89
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-08 23:23:20 +0100

    Update file

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -96,15 +96,19 @@ class Rectangle (Base):
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
     
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """This function that assigns argument"""
-        if len(args) >= 1:
+        if len(args) > 0:
             self.id = args[0]
-        if len(args) >= 2:
+        if len(args) > 1:
             self.width = args[1]
-        if len(args) >= 3:
+        if len(args) > 2:
             self.height = args[2]
-        if len(args) >= 4:
+        if len(args) > 3:
             self.x = args[3]
-        if len(args) >= 5:
+        if len(args) > 4:
             self.y = args[4]
+        
+        if len(args) == 0 or len(kwargs) > 0:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
