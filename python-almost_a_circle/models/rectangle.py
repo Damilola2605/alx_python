ea8f3ffbf4c8e3f6cb9b47f731b8808d0da02a15
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-08 15:28:21 +0100

    First rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from models.base import Base
 """
 Module on a Rectangle class that inherits from Base
 """
+from models.base import Base
+
 class Rectangle (Base):
     """
     This is the calss defining the Rectangle
