71b1aa2c1a70cf47a8a8287ad9a29f6cb9a35e70
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-08 21:45:19 +0100

    Validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -37,6 +37,10 @@ class Rectangle (Base):
     @width.setter
     def width(self, value):
         # Implement any validation logic you need
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -46,6 +50,10 @@ class Rectangle (Base):
     @height.setter
     def height(self, value):
         # Implement validation logic you need
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -55,6 +63,10 @@ class Rectangle (Base):
     @x.setter
     def x(self, value):
         # Implement validation logic you need
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -64,4 +76,8 @@ class Rectangle (Base):
     @y.setter
     def y(self, value):
         # Implement validation logic you need
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
