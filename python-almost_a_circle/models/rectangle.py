cf4b2551a8394c162b584c1357e7c2e267855b3d
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-08 22:14:58 +0100

    Display #1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -88,8 +88,10 @@ class Rectangle (Base):
     
     def display(self):
         """ Function for the display  """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
     
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
