e45034d693b2ca9e73eda88d445d89376d6f1022
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-10 22:36:59 +0100

    POST an email

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This module takes a URl code and email address and display response
+"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: {} <URL> <email>".format(sys.argv[0]))
+        sys.exit(1)
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {
+        'email': email
+    }
+
+    response = requests.post(url, data=data)
+    print(response.text)
