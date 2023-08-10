a8f252ed0dcbf145884ea535f74f5a7f5170505e
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-10 22:58:55 +0100

    404 error

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -9,10 +9,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    response = requests.get(url)
-
-    if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
-    else:
-        print("Response body:")
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for HTTP error status codes
         print(response.text)
+    except requests.exceptions.HTTPError as err:
+        print("Error code:", err.response.status_code)
