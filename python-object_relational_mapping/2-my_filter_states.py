9a6733edadc1c75b2981ce1a2c69dc54b5c6e218
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-22 22:29:54 +0100

    Filter states by user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This Module takes an argument and diaplay all values of a state
+"""
+import MySQLdb
+import sys
+
+
+def search_states(username, password, database, state_name):
+    """Connect to the MySQL server"""
+    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+    """Create a cursor object to interact with the database"""
+    cursor = db.cursor()
+    """Execute the SQL query to retrieve states with matching name"""
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+    """Fetch all the rows from the result set"""
+    results = cursor.fetchall()
+    """Print the results"""
+    for row in results:
+        print(row)
+    """Close the cursor and the database connection"""
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python script_name.py <username> <password> <database> <state_name>")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        state_name = sys.argv[4]
+        search_states(username, password, database, state_name)
