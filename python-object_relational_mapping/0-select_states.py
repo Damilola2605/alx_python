870ed0b528d4c6503c0529384a3705975be7f120
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-22 15:23:00 +0100

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,23 +5,24 @@ This module list the states in the hbtn database
 import MySQLdb
 import sys
 
+
 def list_states(username, password, database):
     """
     This function is to connect Mysql server
     """
     db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
-    #Creating a cursor object for interation in database
+    """Creating a cursor object for interation in database"""
     cursor = db.cursor()
-    #Executing the SQL quary to get the list of states
+    """Executing the SQL quary to get the list of states"""
     quary = "SELECT * FROM states ORDER BY id ASC"
     cursor.execute(quary)
-    #Get all the rows in the result set
+    """Get all the rows in the result set"""
     results = cursor.fetchall()
-    #Print the results
+    """Print the results"""
     for row in results:
         print(row)
 
-    #Stop the cursor and database connection
+    """Stop the cursor and database connection"""
     cursor.close()
     db.close()
 
@@ -32,4 +33,4 @@ if __name__ == "__main__":
         username = sys.argv[1]
         password = sys.argv[2]
         database = sys.argv[3]
-        list_states(username,password,database)
+        list_states(username, password, database)
