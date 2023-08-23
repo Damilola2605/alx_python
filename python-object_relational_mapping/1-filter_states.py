361d638aa3a7870f93ba615f10dbb0faa073048a
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-23 11:30:38 +0100

    List states with N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,31 +2,28 @@
 """
 This module list all state that starts with N
 """
+
 import MySQLdb
 import sys
 
-
 def list_states_with_N(username, password, database):
-    """
-    This function list states with uppercase N
-    """
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-    """Creating a cursor object for interation in database"""
+    """Connect to the MySQL server"""
+    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+
+    """Create a cursor object to interact with the database"""
     cursor = db.cursor()
-    """Execute the SQL query to retrieve states starting with 'N'"""
-    query = "SELECT * FROM states WHERE name LIKE 'N' ORDER BY id ASC"
+
+    """Execute the SQL query to retrieve states starting with 'N' """
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
     cursor.execute(query)
-    """Get all the rows from the result set"""
+
+    """Fetch all the rows from the result set"""
     results = cursor.fetchall()
+
     """Print the results"""
     for row in results:
         print(row)
+
     """Close the cursor and the database connection"""
     cursor.close()
     db.close()
