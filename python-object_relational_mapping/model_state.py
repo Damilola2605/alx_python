197391c28b0c28a44190b110654ca51e1a629a68
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-24 20:06:56 +0100

    First state model

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This is a module that creates python file
+"""
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+"""Create an engine to connect to the MySQL server"""
+engine = create_engine('mysql://username:password@localhost:3306/database_name')
+
+"""Create a declarative base"""
+Base = declarative_base()
+
+"""Define the State class"""
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+"""Create tables in the database"""
+Base.metadata.create_all(engine)
+
+"""Create a session"""
+Session = sessionmaker(bind=engine)
+session = Session()
+
+"""Example usage: Creating a new state"""
+new_state = State(name='New York')
+session.add(new_state)
+session.commit()
+session.close()
