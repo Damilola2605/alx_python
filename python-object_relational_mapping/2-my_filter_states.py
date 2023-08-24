1440e24f6a646c30548250c377acdea8e3450f8d
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-24 11:01:45 +0100

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,17 +7,28 @@ import sys
 
 def search_states(username, password, database, state_name):
     """Connect to the MySQL server"""
-    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
-    """Create a cursor object to interact with the database"""
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    """Create a cursor object to interact with the database'"""
     cursor = db.cursor()
+
     """Execute the SQL query to retrieve states with matching name"""
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    cursor.execute(query)
+
     """Fetch all the rows from the result set"""
     results = cursor.fetchall()
+
     """Print the results"""
     for row in results:
         print(row)
+
     """Close the cursor and the database connection"""
     cursor.close()
     db.close()
