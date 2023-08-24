a50eed12aecf50be01a13c2189e639b67e7dcdd4
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-24 19:18:02 +0100

    All cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -23,6 +23,7 @@ def list_cities_by_state(username, password, database, state_name):
     """Execute the SQL query to retrieve cities of the specified state"""
     query = ("SELECT cities.id, cities.name, states.name "
              "FROM cities "
+             "JOIN states ON cities.state_id = states.id "
              "WHERE states.name = %s "
              "ORDER BY cities.id ASC")
     cursor.execute(query, (state_name,))
