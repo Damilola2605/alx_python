6e70f42aa55f31ca9fb43ae057077dfeb9d9389e
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-24 21:17:05 +0100

    Contains `a`

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This Module lists all state with a
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def list_states_with_letter_a(username, password, db_name):
+    # Create an engine to connect to the MySQL server
+    engine = create_engine(
+        f'mysql://{username}:{password}@localhost:3306/{db_name}'
+    )
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Retrieve and display State objects with the 'a' sorted by ID
+    states_with_a = session.query(State)\
+    .filter(State.name.like('%a%'))\
+    .order_by(State.id)\
+    .all()
+    for state in states_with_a:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python filter_states.py <username> <password> <db_name>")
+        sys.exit(1)
+
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states_with_letter_a(username, password, db_name)
