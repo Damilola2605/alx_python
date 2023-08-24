8953faa565883c36bcd95fec27714c6d190b299c
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-24 12:54:14 +0100

    Cities by states

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Module lists cities and execute the quary
+"""
+import MySQLdb
+import sys
+
+
+def list_cities(username, password, database):
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute the SQL query to retrieve all cities and order by id
+    query = "SELECT * FROM cities ORDER BY id ASC"
+    cursor.execute(query)
+
+    # Fetch all the rows from the result set
+    results = cursor.fetchall()
+
+    # Print the results
+    for row in results:
+        print(row)
+
+    # Close the cursor and the database connection
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script_name.py <username> <password> <database>")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        list_cities(username, password, database)
