e4555d5ca969cf0295c2dfad19bf38b2bc8d4f2a
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-28 21:21:46 +0100

    HBNB

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Import from  flask and start a 
+a web application"""
+
+from flask import Flask
+
+
+"""Create the instancce of the app"""
+app = Flask(__name__)
+
+"""The route for the homepage"""
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+# Define a route for /hbnb
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+if __name__ == '__main__':
+    # Run the app on 0.0.0.0:5000
+    app.run(host='0.0.0.0', port=5000)
