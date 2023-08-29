257a5a66853ba08ad9b97f224ebdded9ba30d541
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-29 22:24:29 +0100

    Number template

---
+++ python-web_framework/5-number_template.py
@@ -0,0 +1,52 @@
+"""
+This script creates a simple Flask web application.
+"""
+
+from flask import Flask, escape, render_template
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """
+    This function defines the route '/' which displays "Hello HBNB!".
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    This function defines the route '/hbnb' which displays "HBNB".
+    """
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """
+    This function defines the route '/c/<text>' which displays "C " followed by the value of the text variable.
+    """
+    return "C " + escape(text.replace("_", " "))
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool(text):
+    """
+    This function defines the route '/python/' and '/python/<text>' which displays "Python " followed by the
+    value of the text variable.
+    """
+    return "Python " + escape(text.replace("_", " "))
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_number(n):
+    """
+    This function defines the route '/number/<n>' which displays "<n> is a number" if n is an integer.
+    """
+    return "{} is a number".format(n)
+
+"""This function links an html file."""
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template("5-number.html", n=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
