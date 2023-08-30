60ac653dc822e28f81df05ecc7f93d84201c063e
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-30 13:11:01 +0100

    Is it a number

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -46,7 +46,11 @@ def is_number(n):
 """This function links an html file."""
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    return render_template("5-number.html", n=n)
+    if isinstance(n, int):
+        return render_template("5-number.html", n=n)
+    else:
+        return "Invalid input. Please provide an integer."
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
